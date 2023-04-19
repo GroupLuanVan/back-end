d40434a19b21fd124466f1ec27fd833527b1b8c1
UyenPhuong <phuongb1807662@student.ctu.edu.vn>
2023-04-19 17:03:01 +0700

    Chinh sua login: tra ve candidate/company

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const Candidate = require('../models/Candidate');
 const Company = require('../models/Company');
-// const Resume = require('../models/Resume');
+const Resume = require('../models/Resume');
 const jwk = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { filterSkipField } = require('../middlewares/common');
@@ -81,16 +81,85 @@ exports.login = async (req, res, next)=>{
     }
 
     if(bcrypt.compareSync(req.body.password, user.password)){
-        const token = jwk.sign({userId: user._id, isAdmin: user.isAdmin}, process.env.APP_SECRET);
-        res.status(200).json({
-            status: 'success',
-            data: {token, user}
+      const token = jwk.sign({userId: user._id, isAdmin: user.isAdmin}, process.env.APP_SECRET);
+      
+      if(user.role == "candidate"){
+        let candidate = await Candidate.findOne({ email: user.email });
+        return res.status(200).json({
+          status: 'success',
+          data: {token, user, candidate}
         })
+      }else if(user.role == "recruiter"){
+        let company = await Company.findOne({ email: user.email });
+
+        return res.status(200).json({
+          status: 'success',
+          data: {token, user, company}
+        })
+      }
+
+
+
+      
+
+
+
     }else{
         //Error: password is not correct
         return res.status(404).json("Sai mật khẩu");
     }
     }catch(error){
+      console.log(error)
       return res.status(404).json("Đăng nhập không thành công");
 }
+
+
+
+
+
+// try {
+
+//   const user = await User.findOne({ email: req.body.email });
+
+//   if (!user) return res.status(404).json("Email chưa được đăng ký");
+
+//   const isPasswordCorrect = await bcrypt.compare(
+//     req.body.password,
+//     user.password
+//   );
+//   if (!isPasswordCorrect)
+//     return res.status(404).json("Sai mật khẩu");
+//     ;
+
+//     const token = jwk.sign({userId: user._id, isAdmin: user.isAdmin}, process.env.APP_SECRET);
+
+//   let resUser = user._doc;
+//   if (user.role == "recruiter") {
+//     const recDetail = await Company.findOne({ userId: user._id }).select({
+//       _id: 0
+//     });
+//     resUser = { ...resUser, ...recDetail._doc };
+//   } else if (user.role == "candidate") {
+//     let candidate = await Candidate.findOne({ userId: user._id })
+//     const activeCvId = await Resume.findOne({
+//       candidateId: candidate._id,
+//     }).select("_id");
+
+//     candidate = filterSkipField(candidate._doc, "_id")
+//     if (activeCvId) {
+//       resUser = { ...resUser, ...candidate, activatedCvId: activeCvId._id, };
+//     } else {
+//       resUser = { ...resUser, ...candidate };
+//     }
+
+//   }
+
+//   delete resUser.password;
+
+
+
+// } catch (err) {
+//   console.log("---------", err)
+//   return res.status(404).json("Đăng nhập không thành công");
+// }
 };
--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -1,5 +1,59 @@
 const Candidate = require ('../models/Candidate');
 
-exports.updateCandidateProfile = async (req, res, next) => {
-    
-  };
+exports.cancelapplyjob = async (req, res, next) => {
+    //create a contact
+  try {
+    //req: jobId, recId, resumeId, hrId
+    const { jobId } = req.body;
+    console.log(req.user);
+    const loggedUser = await User.findById(req.user.id);
+    if (!loggedUser) return next(createError(400, "Không tìm thấy user"));
+
+    let candidate = await Candidate.findOneAndUpdate(
+      { userId: loggedUser.id },
+      { $pull: { applyJobs: jobId } },
+      { new: true }
+    );
+    //find and remove contact
+    await Contact.deleteOne({
+      candidateId: candidate._id,
+      jobPostId: jobId._id,
+    });
+    res.status(200).json({ applyJobs: [...candidate.applyJobs] });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+  
+exports.getUserProfileCvData = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user === null) return next(createError(404, "Khong tim thay User"));
+    let userDetail = {};
+    let candidate = await Candidate.findOne({ userId: user._id });
+    let candidateProfile = candidate.profile;
+
+    if (candidateProfile) {
+      candidate = filterSkipField(candidate._doc, "profile");
+      candidateProfile = filterSkipField(candidateProfile._doc, "_id");
+      userDetail = { ...candidate, ...candidateProfile };
+    } else {
+      userDetail = { ...candidate };
+    }
+    res.status(200).json({ ...user._doc, ...userDetail });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const allCandidate = await Candidate.find();
+
+    res.status(200).json(allCandidate);
+  } catch (err) {
+    next(err);
+  }
+}
