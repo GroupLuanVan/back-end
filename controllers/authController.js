44493e43c5656ad946df570fe7214033c65a88cc
UyenPhuong <phuongb1807662@student.ctu.edu.vn>
2023-04-19 21:01:49 +0700

    Chinh sua Login: tra ve admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -98,7 +98,10 @@ exports.login = async (req, res, next)=>{
         })
       }
 
-
+      return res.status(200).json({
+          status: 'success',
+          data: {token, user}
+      });
 
       
 
