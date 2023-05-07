e2320a66c6350752d8d45b30d8488a2d49ec38a4
UyenPhuong <phuongb1807662@student.ctu.edu.vn>
2023-05-07 11:31:28 +0700

    sap xep address theo a-z

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,10 +1,19 @@
 const Address = require ('../models/Address');
 exports.getAllAddress = async (req, res, next) => {
     try {
-      const address = await Address.find();
+      const address = await Address.find().sort({'title':1});
       res.status(200).json(address);
     } catch (err) {
         next(err);
     }
   };
 
+  // exports.createAAddress = async (req, res, next) => {
+  //   try {
+  //     const address = new Address(req.body)
+  //     await address.save();
+  //     res.status(200).json(address);
+  //   } catch (err) {
+  //     next(err);
+  //   }
+  // };
--- routes/addressRoute.js
+++ routes/addressRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const {getAllAddress} = require('../controllers/addressController.js');
+const {getAllAddress, createAAddress} = require('../controllers/addressController.js');
 
 const addressRouter = express.Router();
 
 
 addressRouter.route('/').get(getAllAddress);
+// addressRouter.route('/add').post(createAAddress);
 
 module.exports = addressRouter;
